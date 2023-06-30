4d9f6a4273bb846c8834ed57323e1c216973b4f3
Richard <richard_friedrichsen@yahoo.de>
2023-06-30 09:32:01 +0200

    docs for models added

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,16 @@ from django.db import models
 
 
 class Question(models.Model):
+    """
+    A class representing a question.
+
+    Attributes:
+        question_text (str): The text of the question.
+        pub_date (datetime): The date when the question was published.
+
+    Methods:
+        __str__(): Returns the string representation of the question.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
@@ -12,6 +22,18 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    """
+    A class representing a choice for a question.
+
+    Attributes:
+        question (Question): The question to which this choice belongs.
+        choice_text (str): The text of the choice.
+        votes (int): The number of votes received for this choice.
+
+    Methods:
+        __str__(): Returns the string representation of the choice.
+
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
